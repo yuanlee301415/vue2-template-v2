806f0e47fe7284f631eb7280e471bde031aabe94
yuanlee <yuanlee@163.com>
2023-04-30 12:13:05 +0800

    chore: 将 css 代码转换为 less 代码

--- src/components/NavBar/NavItem.vue
+++ src/components/NavBar/NavItem.vue
@@ -40,74 +40,87 @@ export default {
 };
 </script>
 
-<style scoped>
-dt {
-  position: relative;
-  cursor: pointer;
-}
-dt > a,
-dt > span {
-  color: white;
-  text-decoration: none;
-  font-size: 16px;
-  display: block;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  height: 40px;
-  overflow: hidden;
-  line-height: 40px;
-  padding-right: 10px;
-}
-dt > i {
-  text-indent: 0 !important;
-  box-sizing: border-box;
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  border-radius: 5px;
-  right: 10px;
-  top: 50%;
-  opacity: 1;
-  transition: all 0.3s;
-  color: rgba(0, 0, 0, 0.85);
-  transform: translateY(-50%);
-  line-height: 40px;
-  background: rgba(255, 255, 255, 0.2);
-}
-dt > i::before {
-  background-color: #fff;
-  transform: rotate(45deg) translateX(2.5px);
-  position: absolute;
-  left: 43.33%;
-  top: 43.33%;
-  width: 6px;
-  height: 1.5px;
-  border-radius: 2px;
-  content: "";
-  box-sizing: border-box;
-}
-dt > i:after {
-  background-color: #fff;
-  transform: rotate(-45deg) translateX(-2.5px);
-  position: absolute;
-  left: 43.33%;
-  top: 43.33%;
-  width: 6px;
-  height: 1.5px;
-  content: "";
-  border-radius: 2px;
-  box-sizing: border-box;
-}
-dl > dd {
-  display: none;
-}
-dl.open > dt > i:before {
-  transform: rotate(-45deg) translateX(2.5px);
-}
-dl.open > dt > i:after {
-  transform: rotate(45deg) translateX(-2.5px);
-}
-dl.open > dd {
-  display: block;
+<style scoped lang="less">
+@dt-height: 40px;
+dl {
+  dt {
+    position: relative;
+    cursor: pointer;
+
+    > a,
+    > span {
+      color: white;
+      text-decoration: none;
+      font-size: 16px;
+      display: block;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      height: @dt-height;
+      overflow: hidden;
+      line-height: @dt-height;
+      padding-right: 10px;
+    }
+
+    > i {
+      text-indent: 0 !important;
+      box-sizing: border-box;
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      border-radius: 5px;
+      right: 10px;
+      top: 50%;
+      opacity: 1;
+      transition: all 0.3s;
+      color: rgba(0, 0, 0, 0.85);
+      transform: translateY(-50%);
+      line-height: @dt-height;
+      background: rgba(255, 255, 255, 0.2);
+
+      &::before {
+        background-color: #fff;
+        transform: rotate(45deg) translateX(2.5px);
+        position: absolute;
+        left: 43.33%;
+        top: 43.33%;
+        width: 6px;
+        height: 1.5px;
+        border-radius: 2px;
+        content: "";
+        box-sizing: border-box;
+      }
+
+      &:after {
+        background-color: #fff;
+        transform: rotate(-45deg) translateX(-2.5px);
+        position: absolute;
+        left: 43.33%;
+        top: 43.33%;
+        width: 6px;
+        height: 1.5px;
+        content: "";
+        border-radius: 2px;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  > dd {
+    display: none;
+  }
+
+  &.open {
+    > i:before {
+      transform: rotate(-45deg) translateX(2.5px);
+    }
+
+    > i:after {
+      transform: rotate(45deg) translateX(-2.5px);
+    }
+
+    > dd {
+      display: block;
+    }
+  }
 }
 </style>
