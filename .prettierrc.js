637a8f7cb9caece6693d6c3f20058afb77cd0692
Yuan Lee <yuanlee301415@126.com>
2024-06-04 16:29:36 +0800

    chore: Add prettier 基础配置

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    $schema: 'https://json.schemastore.org/prettierrc',
+    semi: false, // 禁止：自动分号插入 (ASI)
+    tabWidth: 4,
+    singleQuote: true, // 使用单引号
+    printWidth: 150, // 换行长度
+    trailingComma: 'none'
+}
