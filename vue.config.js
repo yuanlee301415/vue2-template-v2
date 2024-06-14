125674f211d01665fbb3a5c99b878dc0f0b90858
Yuan Lee <yuanlee301415@126.com>
2024-06-14 16:33:58 +0800

    chore: 代码拆分

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
 const { version } = require('./package.json')
+const path = require('path')
 
 process.env.VUE_APP_VERSION = [version, process.env.VUE_APP_BUILD_VERSION].join('.')
 process.env.VUE_APP_BUILD_DATE = new Date().toISOString()
@@ -45,5 +46,30 @@ module.exports = defineConfig({
     chainWebpack: (config) => {
         // 构建进度
         config.plugin('progress').use(new webpack.ProgressPlugin())
+        if (process.env.NODE_ENV === 'production') {
+            // 代码拆分
+            config.optimization.splitChunks({
+                chunks: 'all',
+                cacheGroups: {
+                    libs: {
+                        name: 'chunk-libs',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: 10,
+                        chunks: 'initial'
+                    },
+                    components: {
+                        name: 'chunk-components',
+                        test: path.resolve(__dirname, 'src/components'),
+                        priority: 20,
+                        reuseExistingChunk: true // 如果当前 chunk 包含已从主 bundle 中拆分出的模块，则它将被重用，而不是生成新的模块。
+                    },
+                    elementUI: {
+                        name: 'chunk-elementUI',
+                        test: /[\\/]node_modules[\\/]element-ui/,
+                        priority: 100
+                    }
+                }
+            })
+        }
     }
 })
